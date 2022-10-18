37eb8c5300e641a5408cefbfc86a4b775a9596b1
Chasezaiss <44202321+Chasezaiss@users.noreply.github.com>
2022-10-18 16:29:12 -0500

    Add files via upload

---
+++ ChristmasLights/index.js
@@ -0,0 +1,11 @@
+//alert("hello");
+
+
+    //$(".light").fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
+
+/*$(document).ready(function () {
+
+    $('body').animate({ backgroundColor: '#ffcc00' }).wait(500)
+        .animate({ backgroundColor: '#eeeeee' }).wait(500)
+        .animate({ backgroundColor: '#3b5998' }).wait(500)
+});*/
